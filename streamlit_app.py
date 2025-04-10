57cfc0c1e984baf28b51fe7d7536750aa121cc8c
Scott Murray <scott.murray@powerschool.com>
2025-04-09 20:28:33 -0600

    refactor(root): Adding search term

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,8 +36,7 @@ if ingredients_list and name_on_order:
 
     for fruit_chosen in ingredients_list:
         search_on = my_dataframe.filter(col('FRUIT_NAME') == fruit_chosen).select(col('SEARCH_ON')).collect()[0][0]
-        st.write(f'You chose: {fruit_chosen} with search term: {search_on}')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     time_to_insert = st.button('Submit Order')
